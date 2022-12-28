b65162867abf26c24983226317dedd2bcbde2781
sanuki0679 <sanuki0679@gmail.com>
2022-12-28 19:02:52 +0900

    04/02_if.phpの完成

--- 04/02_if.php
+++ 04/02_if.php
@@ -4,3 +4,10 @@ echo '$pointの値を入力して下さい: ';
 $point = trim(fgets(STDIN));
 
 // ここに処理を記述
+if (( $point % 2 ) == 0 ) {
+    echo $point ; 
+    echo 'は偶数です' .PHP_EOL; 
+} else {
+    echo $point ;
+    echo 'は奇数です' .PHP_EOL;
+}
